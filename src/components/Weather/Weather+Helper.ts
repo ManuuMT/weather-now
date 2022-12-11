bfd5c26fa9970e353b957b1f772b2ddda11b8899
Emanuel Maldonado <ema14mt@gmail.com>
2022-12-11 04:04:10 -0300

    Loader, fahrenheit, and styles added

---
+++ src/components/Weather/Weather+Helper.ts
@@ -0,0 +1,5 @@
+export const weatherBg = {
+  Sun: "https://i.imgur.com/ziKZEg0.gif",
+  Rain: "https://i.imgur.com/gdbaw1T.gif",
+  Snow: "https://i.imgur.com/n9PZVZh.gif",
+};
--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Weather.scss";
 import axios from "axios";
+import { weatherBg } from "./Weather+Helper";
 
 interface WeatherProps {
   city: string;
@@ -8,63 +9,83 @@ interface WeatherProps {
 
 const Weather: React.FC<WeatherProps> = (props) => {
   // * States
-  const [state, setState] = useState<any>();
+  const [data, setData] = useState<any>();
+  const [isCelsius, setIsCelsius] = useState(true);
 
   // * Methods
   const SetBackground = () => {
-    const temp = Number(state.current.temp_c);
-    if (temp > 20)
-      return "https://media0.giphy.com/media/5K4WeEfVwG6tWl0FBW/giphy.gif?cid=790b7611d5c9ca58dadfaecb719a8f2d20974846cbcb9050&rid=giphy.gif&ct=g";
-    return "https://media3.giphy.com/media/cnL3XuYJy9WJYiLGMY/giphy.gif";
+    const temp = Number(data.current.temp_c);
+    if (temp >= 20) return weatherBg.Sun;
+    if (temp > 5 && temp < 20) return weatherBg.Rain;
+    return weatherBg.Snow;
   };
+
+  const Loader = () => (
+    <div className="weather-loader">
+      <img
+        className="weather-loader-img"
+        src="https://i.imgur.com/l8D5JlD.png"
+        alt="Loader"
+      />
+    </div>
+  );
 
   // * Life Cycle
   useEffect(() => {
     const GetWeatherData = async (): Promise<any> => {
       const url = `${process.env.REACT_APP_URL}key=${process.env.REACT_APP_KEY}&q=${props.city}&aqi=no`;
       const res = await axios.get(url);
-      setState(res.data);
+      //   setTimeout(() => setData(res.data), 30000);
+      setData(res.data);
     };
     GetWeatherData();
   }, []);
 
-  useEffect(() => console.log(state), [state]);
+  useEffect(() => console.log(data), [data]);
 
   return (
-    <div className="weather">
-      {state && (
-        <>
-          <div className="weather-background">
-            <img
-              className="weather-background-img"
-              src={SetBackground()}
-              alt="weather-background"
-            />
-          </div>
-          <div className="weather-date">
-            <div className="weather-day">
-              <p>{state.location.localtime.split(" ")[0]}</p>
+    <div className="weather-container">
+      <div className="weather">
+        {!data && Loader()}
+        {data && (
+          <>
+            <div className="weather-background">
+              <img
+                className="weather-background-img"
+                src={SetBackground()}
+                alt="weather-background"
+              />
             </div>
-            <div className="weather-time">
-              <p>{state.location.localtime.split(" ")[1]}</p>
+            <div className="weather-date">
+              <div className="weather-day">
+                <p>{data.location.localtime.split(" ")[0]}</p>
+              </div>
+              <div className="weather-time">
+                <p>{data.location.localtime.split(" ")[1]}</p>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-      <div className="weather-info">
-        <div className="weather-city">
-          <h3>{props.city}</h3>
-        </div>
-        {state ? (
-          <div className="weather-temp">
-            <h1>{state.current.temp_c}ºC</h1>
-          </div>
-        ) : (
-          <h1>Loading...</h1>
+
+            <div className="weather-info">
+              <div className="weather-city">
+                <h2>{props.city}</h2>
+              </div>
+
+              <div
+                className="weather-temp"
+                onClick={() => setIsCelsius(!isCelsius)}
+              >
+                <h1>
+                  {isCelsius
+                    ? `${data.current.temp_c}ºC`
+                    : `${data.current.temp_f}ºF`}
+                </h1>
+              </div>
+            </div>
+            <div className="weather-msg">
+              <p>Developed by Emanuel</p>
+            </div>
+          </>
         )}
-      </div>
-      <div className="weather-msg">
-        <p>Developed by Emanuel</p>
       </div>
     </div>
   );
